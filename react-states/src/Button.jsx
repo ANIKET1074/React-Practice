bd86d9661dd32ef8c466acdbcff2269f900860b2
JADAV ANIKET KAMLESHKUMAR <86431201+ANIKET1074@users.noreply.github.com>
2023-11-25 17:38:21 +0530

    Button Component added

---
+++ react-states/src/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ action }) => {
+  return (
+    <>
+      <button onClick={action}>Buy New Ticket</button>
+    </>
+  );
+};
+
+export default Button;
